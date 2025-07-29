9765423bfdd387cc3286365d7183e056855105f4
cgartco6 <155203611+cgartco6@users.noreply.github.com>
2025-07-29 09:55:00 +0200

    Create __init__.py

---
+++ agents/__init__.py
@@ -0,0 +1,5 @@
+from .signal_optimizer import SignalOptimizer
+from .performance_analyzer import PerformanceAnalyzer
+from .market_observer import MarketObserver
+
+__all__ = ['SignalOptimizer', 'PerformanceAnalyzer', 'MarketObserver']
